42e7d51ba85e02142f741c14b4eb24d2e6a78ac2
Chris Stouff <chris.stouff@armor.com>
2023-05-12 22:05:16 -0500

    Changed logic to `git show -m`

--- scripts/utilities/verify-files-changed.sh
+++ scripts/utilities/verify-files-changed.sh
@@ -4,18 +4,14 @@ function main() {
   local changeset
   local -r base_directory="$1"
   local -r grep_pattern="$2"
-  local -r current_hash="$(git rev-parse HEAD)"
-  local -r previous_hash="$(git rev-list --tags --skip=1 --max-count=1)"
-  local -r previous_version="$(git describe --abbrev=0 --tags "$previous_hash")"
 
-  echo "Most recent tag was $previous_version at commit $previous_hash"
   echo "Complete changeset:"
 
-  git --no-pager diff --name-only "$previous_hash..$current_hash"
+  git --no-pager show -m --name-only
 
   echo ""
   echo "Looking for files in ./${base_directory}/ matching '${grep_pattern}'"
-  changeset="$(git --no-pager diff --name-only "$previous_hash..$current_hash")"
+  changeset="$(git --no-pager show -m --name-only)"
   changeset="$(echo "$changeset" | grep -E "^$base_directory/.*")"
   changeset="$(echo "$changeset" | grep -E "$grep_pattern" || true)"
 
